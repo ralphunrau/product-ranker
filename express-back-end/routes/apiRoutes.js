4589fe22f39719cc935a47403b155ea64bf88034
Ralph Unrau <xralphunrau@gmail.com>
2022-05-13 10:52:14 -0700

    Remove axios fetch from rapid API

--- express-back-end/routes/apiRoutes.js
+++ express-back-end/routes/apiRoutes.js
@@ -5,21 +5,6 @@ const axios = require('axios');
 
 router.get('/data', (req, res) => {
 
-  const options = {
-    method: 'GET',
-    url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
-    params: {keyword: 'phone', country: 'US', category: 'aps'},
-    headers: {
-      'X-RapidAPI-Host': 'amazon-product-reviews-keywords.p.rapidapi.com',
-      'X-RapidAPI-Key': '67d33be7eemsh31da64a18d64453p1ef428jsnbf5c166c363a'
-    }
-  };
-  
-  axios.request(options).then((req, res) => {
-    console.log(response.data);
-  }).catch((err) => {
-    console.error(err);
-  });
-});
+})
 
 module.exports = router;
