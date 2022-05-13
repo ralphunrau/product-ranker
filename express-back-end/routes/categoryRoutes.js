521761cf2c2ea50760090ad194cd357fab74ba9a
amuliawijaya23 <amuliawijaya@gmail.com>
2022-05-12 21:45:47 -0700

    adds knex to routers

--- express-back-end/routes/categoryRoutes.js
+++ express-back-end/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require ('../db/knexfile');
+const knex = require('../db/knex');
 
 router.get('/', (req, res) => {
   console.log(res);
