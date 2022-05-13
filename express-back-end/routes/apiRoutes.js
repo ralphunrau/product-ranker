f2d87363fbdc6d30003049dcf0600f014c7a9a49
Ralph Unrau <xralphunrau@gmail.com>
2022-05-13 10:35:52 -0700

    Add axios request to api

--- express-back-end/routes/apiRoutes.js
+++ express-back-end/routes/apiRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const axios = require('axios');
 
 router.get('/data', (req, res) => {
-  res.json({message: "Seems to work!"});
+
+  const options = {
+    method: 'GET',
+    url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
+    params: {keyword: 'phone', country: 'US', category: 'aps'},
+    headers: {
+      'X-RapidAPI-Host': 'amazon-product-reviews-keywords.p.rapidapi.com',
+      'X-RapidAPI-Key': '67d33be7eemsh31da64a18d64453p1ef428jsnbf5c166c363a'
+    }
+  };
+  
+  axios.request(options).then((req, res) => {
+    console.log(response.data);
+  }).catch((err) => {
+    console.error(err);
+  });
 });
 
 module.exports = router;
--- express-back-end/routes/productRoutes.js
+++ express-back-end/routes/productRoutes.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
+router.get('/show', (req, res) => {
+})
 
 module.exports = router;
