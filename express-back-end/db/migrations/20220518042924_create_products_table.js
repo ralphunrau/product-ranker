afb80fa119377358f35949b4f482cde8411c3b9f
amuliawijaya23 <amuliawijaya@gmail.com>
2022-05-22 18:47:35 -0700

    fixes add to wish error when data received is too long

--- express-back-end/db/migrations/20220518042924_create_products_table.js
+++ express-back-end/db/migrations/20220518042924_create_products_table.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('products', (table) => {
       table.string('id', 255).notNullable().primary();
-      table.string('image', 255).notNullable();
-      table.string('link', 255).notNullable();
-      table.string('title', 255).notNullable();
+      table.string('image', 500).notNullable();
+      table.string('link', 500).notNullable();
+      table.string('title', 500).notNullable();
       table.string('price', 255).notNullable().defaultTo('Not Available');
       table.decimal('rating').notNullable().defaultTo(0);
       table.integer('ratings_total').notNullable().defaultTo(0);
