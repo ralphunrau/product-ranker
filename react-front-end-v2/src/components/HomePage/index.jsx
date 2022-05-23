77945ed3642a05a1e33e8c6b2bb270afcd688ed6
Ralph Unrau <xralphunrau@gmail.com>
2022-05-23 11:14:32 -0700

    Initializes home page

---
+++ react-front-end-v2/src/components/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import ImageSubmitForm from '../TierList/ImageSubmitForm';
+
+export default function HomePage(props) {
+
+
+
+  return (
+    <div className="home-page">
+      
+    </div>
+  );
+};
